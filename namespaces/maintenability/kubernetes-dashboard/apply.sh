9a3216f4a450bdff230f24b87bb5dbc94bf66222
kyamazawa <yamazawa@supersoftware.co.jp>
2018-05-03 00:58:19 +0900

    wait running

--- namespaces/maintenability/kubernetes-dashboard/apply.sh
+++ namespaces/maintenability/kubernetes-dashboard/apply.sh
@@ -21,3 +21,13 @@ eval "NAMESPACE=${NAMESPACE} ./kubernetes-dashboard-account.yaml.sh" | eval "${K
 
 print_green "kubernetes-dashboard login token is"
 eval "${KUBECTL} get secret \$(${KUBECTL} get secret | grep webmaster | awk '{print \$1}') -o jsonpath='{.data.token}' && echo"
+
+## Wait until kubernetes-dashboard available
+echo 'Wait until kubernetes-dashboard available'
+while :
+do
+  if [ $(eval "${KUBECTL} get po | grep dashboard | awk 'NR==1{print \$3}' | grep -e 'Running'") ]; then
+    break    
+  fi
+  sleep 1s;
+done
--- namespaces/maintenability/weave/apply.sh
+++ namespaces/maintenability/weave/apply.sh
@@ -12,3 +12,13 @@ KUBECTL="kubectl --namespace=\"${NAMESPACE}\""
 
 # Deploy
 eval "./weave-scope.yaml.sh" | eval "${KUBECTL} apply -f -"
+
+## Wait until weave-scope available
+echo 'Wait until weave-scope available'
+while :
+do
+  if [ $(eval "${KUBECTL} get po | grep weave-scope-app | awk 'NR==1{print \$3}' | grep -e 'Running'") ]; then
+    break    
+  fi
+  sleep 1s;
+done
